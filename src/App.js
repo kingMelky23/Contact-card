7c109d145ac7477e4bd446fe38de8adebc70bd33
king <tettehmel@gmail.com>
2020-06-02 21:29:36 -0400

    state excercises

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React, {Component} from 'react';
 import './App.css';
 
-class HelloWord extends Component{
+class HelloWorld extends Component{
   render(){
-    return <h1>Hello {1+5} </h1>;
+    return <h1>Hello World </h1>;
   }
     
 }
 
+class HelloFriend extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      name: props.name,
+    };
+
+    setTimeout(this.updateName.bind(this),2000)
+  }
+
+
+  updateName(){
+    this.setState({name: "Jeff"})
+  }
+
+  render(){
+    return <h1>Hello {this.state.name}</h1>
+  }
+}
+
+
 class App extends Component{
   render(){
-    return <HelloWord/>
-  }
+    return(
+      <>
+        <HelloWorld/>
+        <HelloFriend name = "Mel" />
+      </>
+    )}
 }
 export default App;
