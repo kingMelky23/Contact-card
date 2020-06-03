5e1af0cabf13254c88e007c6c5342da378be183c
king <tettehmel@gmail.com>
2020-06-03 08:55:19 -0400

    created 3 contact cards

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ class App extends Component{
   render(){
     return(
     <>
-      <ContactCard  name="eric" mobile="234554356" work="678543267" email="john@gmail.com"/>
+      <ContactCard  name="eric" mobile="234554356" work="678543267" email="eric@gmail.com"/>
+
+      <ContactCard  name="derick" mobile="269434567" work="678543267" email="eric@gmail.com"/>
+
+      <ContactCard  name="gerric" mobile="45848657" work="678543267" email="eric@gmail.com"/>
     </>
     ); 
   }
--- src/ContactCard.js
+++ src/ContactCard.js
@@ -1,15 +1,14 @@
 import React,{Component} from "react";
-
+import PropTypes from "prop-types"
 class ContactCard extends Component{
     render(){
         return(
             <div>
-                <h1>{this.props.name}
-                </h1> 
+                <h1>{this.props.name}</h1> 
                 <ul>
-        <li>mobile: {this.props.mobile}</li>
-        <li>work: {this.props.work}</li>
-        <li>email: {this.props.email}</li>
+                    <li>mobile: {this.props.mobile}</li>
+                    <li>work: {this.props.work}</li>
+                    <li>email: {this.props.email}</li>
                 </ul>
             </div>
         );
@@ -17,3 +16,10 @@ class ContactCard extends Component{
 }
 
 export default ContactCard
+
+ContactCard.propTypes = {
+    name: PropTypes.string.isRequired,
+    mobile: PropTypes.string.isRequired,
+    work: PropTypes.string,
+    email: PropTypes.string.isRequired,
+}
