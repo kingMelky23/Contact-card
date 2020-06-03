37490e9b46befd5d839e816f9796966827424699
king <tettehmel@gmail.com>
2020-06-02 20:31:34 -0400

    my first react app commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class HelloWord extends Component{
+  render(){
+    return <h1>Hello {1+5} </h1>;
+  }
+    
 }
 
+class App extends Component{
+  render(){
+    return <HelloWord/>
+  }
+}
 export default App;
